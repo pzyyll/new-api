aa990083759996928eb7520abde98e10f3b1fd17
pzyyll <pzyyll@gmail.com>
2025-06-28 17:49:44 +0800

    feat: retry when timeout

--- controller/relay.go
+++ controller/relay.go
@@ -287,10 +287,6 @@ func shouldRetry(c *gin.Context, openaiErr *dto.OpenAIErrorWithStatusCode, retry
 		return true
 	}
 	if openaiErr.StatusCode/100 == 5 {
-		// 超时不重试
-		if openaiErr.StatusCode == 504 || openaiErr.StatusCode == 524 {
-			return false
-		}
 		return true
 	}
 	if openaiErr.StatusCode == http.StatusBadRequest {
